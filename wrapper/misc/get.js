30cff60a5789942188dd0f1c6f1f7a9416a9679e
Tetradual <94416681+Tetradual@users.noreply.github.com>
2022-01-04 01:02:45 -0600

    oops

--- wrapper/misc/get.js
+++ wrapper/misc/get.js
@@ -3,15 +3,25 @@ short version of https.get()
 ***/
 
 const https = require("https");
+const http = require("http");
 
 module.exports = function (url, options = {}) {
 	var data = [];
 	return new Promise((res, rej) => {
-		https.get(url, options, (response) =>
-			response
-				.on("data", (v) => data.push(v)) // push data to object
-				.on("end", () => res(Buffer.concat(data))) // return data
-				.on("error", rej)
-		);
+		try {
+			http.get(url, options, (response) =>
+				response
+					.on("data", (v) => data.push(v)) // push data to object
+					.on("end", () => res(Buffer.concat(data))) // return data
+					.on("error", rej)
+			);
+		} catch(err) {
+			https.get(url, options, (response) =>
+				response
+					.on("data", (v) => data.push(v)) // push data to object
+					.on("end", () => res(Buffer.concat(data))) // return data
+					.on("error", rej)
+			);
+		}
 	});
 };
